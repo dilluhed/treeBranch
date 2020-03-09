3d07922469644e4bf0e2e3e3915c421b1d9b2e51
nxthando <natedillamc@gmail.com>
2020-03-09 13:18:23 -0700

    initial commit

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -3,5 +3,5 @@ public class treeBranchTrunk
     public static void main(String[] args)
     {
         System.out.println("COVID-19 takes Valley View High!");
-    }//end main method
+    }//end main method 
 }//end driver class
