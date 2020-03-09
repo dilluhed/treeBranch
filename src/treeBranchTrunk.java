377fec6382a3a53f68c2768e9d56a379c164d338
nxthando <natedillamc@gmail.com>
2020-03-09 13:29:12 -0700

    Added COVID 19's new advances in calculus class

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -4,5 +4,6 @@ public class treeBranchTrunk
     {
         System.out.println("COVID-19 takes Valley View High!");
         System.out.println("COVID-19 takes AP Calculus!");
+        System.out.println("COVID-19 takes Mr Racatian!");
     }//end main method
 }//end driver class
