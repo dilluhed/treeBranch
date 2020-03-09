9021638581173804331868cace714c69d930b2fd
nxthando <natedillamc@gmail.com>
2020-03-09 13:29:59 -0700

    Added COVID 19's new advances in the world

--- src/treeBranchTrunk.java
+++ src/treeBranchTrunk.java
@@ -5,5 +5,6 @@ public class treeBranchTrunk
         System.out.println("COVID-19 takes Valley View High!");
         System.out.println("COVID-19 takes AP Calculus!");
         System.out.println("COVID-19 takes Mr Racatian!");
+        System.out.println("COVID-19 kills everything....");
     }//end main method
 }//end driver class
